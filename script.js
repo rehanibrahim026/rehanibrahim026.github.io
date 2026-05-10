27cadd0b2c85794d0a789372ef3c9c65cb52bb71
Rehan Ibrahim <rehanibrahim026@student.unsrat.ac.id>
2026-05-10 22:55:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@
 
 function startTypewriter() {
     const el   = document.getElementById('hero-typewriter');
-    const text = 'Mahasiswa Teknik Informatika Semester 4, Fakultas Teknik UNSRAT — tertarik pada pengembangan perangkat lunak dan Game Developer.';
+    const text = 'Mahasiswa Teknik Informatika Semester 4, Fakultas Teknik UNSRAT — tertarik pada bidang Game Development dan UI/UX Design.';
     let i = 0;
 
     el.style.opacity = '1';
